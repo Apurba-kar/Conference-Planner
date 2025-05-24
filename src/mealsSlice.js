73758a48c9f8aa0f25c10910cd2972b5c7e7452a
Apurba-Kar <apurbakar1111@email.com>
2025-05-24 23:12:00 +0530

    Initial commit

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,17 +1,20 @@
-// mealsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const mealsSlice = createSlice({
-  name: 'meals',
+  name: "meals",
   initialState: [
-   
+    { name: "Breakfast", cost: 50, selected: false, type: "mealForPeople" },
+    { name: "High Tea", cost: 25, selected: false, type: "mealForPeople" },
+    { name: "Lunch", cost: 65, selected: false, type: "mealForPeople" },
+    { name: "Dinner", cost: 70, selected: false, type: "mealForPeople" },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+      const index = action.payload;
+      state[index].selected = !state[index].selected;
     },
   },
 });
 
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
